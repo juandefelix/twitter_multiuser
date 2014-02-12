285696e318c32faa0a137486193de2bd6c74a385
Apprentice <apprentice@dbc-chicago-14.local>
2014-02-12 09:21:21 -0600

    added twitter worker rb and User#tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+   def tweet(status)
+    tweet = tweets.create!(:status => status)
+    TweetWorker.perform_async(tweet.id)
+  end
   # Remember to create a migration!
 end
